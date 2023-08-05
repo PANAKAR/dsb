405c67a4ee4a15b40b5e00f8e5eb880c45f1ca6c
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-05 09:36:31 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
 import {motion} from 'framer-motion'
-import {styles} from '../styles'
-import { ComputersCanvas } from './canvas'
 
 function Hero() {
   return (
-    <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className="flex flex-col justify-center items-center mt-20">
-          <div className="w-5 h-5 rounded-full bg-[#fff] "></div>
-          <div className="w-3 sm:h-80 h-40 violet-gradient"></div>
+   <section className="relative w-full h-screen mx-auto">
+      <div className="d-sm-flex align-items-center justify-content-between w-100" style={{ height: "100vh" }}>
+        <div className="col-md-4 mx-auto mb-4 mb-sm-0 headline">
+          <h1 className="col-md-2 display-4 font-weight-bold" id='heading' style={{color: "white"}}>Timeless Trends, Captured in Gems: Welcome to<span style={{ color: "#9B5DE5" }}>Cronus.</span></h1>
+          
         </div>
+        <div className="col-md-8 h-100 clipped" style={{ minHeight: "350px", backgroundPosition: "center", backgroundSize: "cover" }}>
 
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white text-center text-bold mt-20 shadow-3xl`} style={{textShadow: "5px 2px 1px gray"}}>At DSB Media, We Create<span className="text-white" style={{textShadow: "5px 2px 1px gray"}}>, "FOR YOU." </span></h1>
         </div>
-      </div> 
-
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      </div>
+      <div id='director' className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center" style={{background: "none"}}>
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center">
-            <motion.dev 
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-center" style={{background: "none"}}>
+            <motion.div 
               animate={{
                 y:[0,24,0]
               }}
@@ -29,11 +25,12 @@ function Hero() {
                 repeat: Infinity,
                 repeatType: 'loop'
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className='w-3 h-3 rounded-full bg-white mb-1'
             />
           </div>
         </a>
       </div>
+      <About />
     </section>
   )
 }
