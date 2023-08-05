f67568a75db87c65ddaf9d427b02ccdf505efdf7
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-05 09:47:32 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,94 @@
 import React, {useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
 import { styles } from '../styles'
 import { NavLink } from 'react-router-dom'
-import {logo, menu, close} from '../assets'
+import {logo, menu, close, Logo} from '../assets'
 import { navLinks } from '../constants'
+import { BrowserRouter as Router, Link, useNavigate, Route, Routes } from 'react-router-dom'
+import './NavBar.css'
+import Hero from './Hero'
+
 
 function Navbar() {
-  const [active, setActive] = useState("");
-  const [toggle, setToggle] = useState(false);
+  const [click, setClick] = React.useState(false);
 
+  const handleClick = () => setClick(!click);
+  const Close = () => setClick(false);
+  
   return (
-    <nav className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20 bg-black text-black`}>
-      <div className="w-full flex  justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className='flex items-center gap-2' onClick={() => {
-          setActive("");
-          window.scrollTo(0,0);
-        }}>
-          <img src={logo} alt="logo" className='w-12 h-12 object-contain mx-0 rounded-full' />
-          <p className="text-white text-[18px] font-bold cursor-pointer lg:flex sm:block">DSB <span>  | Media </span></p>
-        </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((Link) => (
-            <li
-              key={Link.id}
-              className={`${active === Link.title ? "text-white" : "text-secondary"} hover:text-black text-[25px] font-bold font-medium cursor-pointer`}
-              onClick={() => setActive(Link.title)}
-            >
-              <a href={`#${Link.id}`}>{Link.title}</a>
+    <div>
+     <div className={click ? "main-container" : ""}  onClick={()=>Close()} />
+      <nav className="navbar" onClick={e => e.stopPropagation()}>
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            <img src={Logo} alt="" style={{width: "10%", height: "10%", borderRadius: "100%"}} />
+          </NavLink>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                activeclassname="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+                color={click ? "violet" : "white"}
+              >
+                Home
+              </NavLink>
             </li>
-          ))}
-        </ul>
-        <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle(!toggle)}/>
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                activeclassname="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+                color={click ? "violet" : "white"}
 
-          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 bg-gradient-to-br from-black to-black  absolute top-20 right-0 mx-4 my-2 min-w-[140px] x-10 rounded-xl`}>
-            <ul className="list-none flex justify-end items-start flex-col gap-4 text-white">
-              {navLinks.map((Link) => (
-                <li
-                  key={Link.id}
-                  className={`${active === Link.title ? "text-white" : "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`}
-                  onClick={() => {
-                    setToggle(!toggle);
-                    setActive(Link.title);
-                  }}
-                >
-                  <a href={`#${Link.id}`}>{Link.title}</a>
-                </li>
-              ))}
-            </ul>
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/collections"
+                onClick={click ? handleClick : null}
+                color={click ? "violet" : "white"}
+
+              >
+                Collections
+              </NavLink>
+            </li>
+            <li className="nav-item" style={{ flexDirection: "row", width: "100px"}}>
+              <NavLink
+                to="/Why us"
+                activeclassname="active"
+                className="nav-links"
+               onClick={click ? handleClick : null}
+               color={click ? "violet" : "white"}
+
+              >
+                Why us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                activeclassname="active"
+                className="nav-links"
+               onClick={click ? handleClick : null}
+               color={click ? "violet" : "white"}
+
+              >
+                Contact
+              </NavLink>
+            </li>
+           
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
           </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    </ div>
+  );
 }
 
 export default Navbar
