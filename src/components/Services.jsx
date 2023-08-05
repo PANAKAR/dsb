01443147dc5d0d67fd00d3d7e7a6b432e570825f
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-05 10:14:44 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,35 +7,6 @@ import { SectionWrapper } from '../hoc';
 import './service.css'
 import { asset15, asset16, asset17, asset18, asset20, asset23, asset22, asset4} from '../assets';
 
-const ServiceCard = ({index, title, icon, description}) => {
-    return (
-        <Tilt className="xs:w-[250px] w-full">
-            <motion.div
-                variants={fadeIn("right", 0.5 * index, 0.75)}
-                className="w-full min-h-[300px] p-[1px] rounded-[20px] shadow-card-900"
-                >
-                    <div 
-                        options = {{
-                            max:45,
-                            scale: 1,
-                            speed: 450
-                        }}
-                        className="text-black h-[400px] rounded-[20px] py-6 px-12 min-h-[290px] flex justify-evenly items-center flex-col"
-                        style={{background: "white",
-                            boxShadow: "10px 12px 9px gray" }}
-                    >
-                        <img src={icon} alt={title}
-                        className='w-25 h-25 object-contain'  style={{width: "55%"}} />
-                        <h3 className="text-black text-[20px] font-bold text-center">{title}</h3>
-                        <br />
-                        <h5 className="text-black text-[12px] font-bold text-center">{description}</h5>
-                    </div>
-            </motion.div>
-
-        </Tilt>
-    )
-}
-
 function Services() {
   return (
     <>
