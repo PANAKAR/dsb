7d4609f276299c81f3452421f467d7598801f00a
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-05 09:46:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,25 @@ import { BrowserRouter } from "react-router-dom"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import About from "./components/About"
-import Feedbacks from "./components/Feedbacks"
 import Contact from "./components/Contact"
-import StarsCanvas from "./components/StarsCanvas"
 import Services from "./components/Services"
-import { motion } from "framer-motion"
-import { slideIn } from "./utils/motion"
+import { BrowserRouter as Router,  Link, Route, Routes} from "react-router-dom"
 
 const App = () => {
+  
+
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-black">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="bg-white bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Hero />
         </div>
-        <About/>
-        <Services />
-        <Feedbacks />
-        <div className="relative z-0 ">
-         
-          <Contact classname="z-10" style={{zIndex: "10"}}/>
-          <StarsCanvas className="hidden-xs hidden-m"style={{zIndex: "10"}}/>
-        </div>
+        <Routes>
+          <Route path="/" element={<Hero />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/collections" element={<Services />} />
+          <Route path="/contact" element={<Contact />}/>
+        </Routes>
       </div>
     </BrowserRouter>
   )
