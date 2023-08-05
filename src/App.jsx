a85ab876fc3161f54820d2598d155f64d1ff1948
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-05 09:43:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,7 @@ const App = () => {
         <Services />
         <Feedbacks />
         <div className="relative z-0 ">
-          <motion.div
-            variants={slideIn('left', 'tween', 0.2, 1)}>
-            <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl" style={{width: "1200px"}}>
-              <div class="flex-shrink-0">
-                <img src="https://www.creativefabrica.com/wp-content/uploads/2021/01/27/doodle-exclamation-mark-Graphics-8127361-1.jpg" alt="" className="" style={{width: "95px"}} />
-              </div>
-              <div class="ml-6 pt-1">
-                <h4 class="text-xl text-gray-900">DSB Media</h4>
-                <p class="text-base text-gray-600">Fill out our contact form today and let us bring our top-notch services to your doorstep, tailored to meet your unique needs and unlock new opportunities for success!</p>
-              </div>
-            </div>
-          </motion.div>
+         
           <Contact classname="z-10" style={{zIndex: "10"}}/>
           <StarsCanvas className="hidden-xs hidden-m"style={{zIndex: "10"}}/>
         </div>
