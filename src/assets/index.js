673b3c0683894e60f3d8d941d297848e229ff5c4
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-05 09:39:44 +0530

    added assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -30,9 +30,42 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
+import Logo from "./logo.jpg"
+import hero from "./asset9-removebg.png"
+import asset13 from './asset13.jpeg'
+import asset23 from './asset23.jpeg'
+import asset3 from './asset3.jpeg'
+import asset15 from './asset15.jpeg'
+import asset16 from './asset16.jpeg'
+import asset17 from './asset17.jpeg'
+import asset18 from './asset18.jpeg'
+import asset25 from './asset25.jpeg'
+import asset20 from './asset20.jpeg'
+import asset22 from './asset22.jpeg'
+import asset4 from './asset4.jpeg'
+import asset6 from './asset6.jpeg'
+import asset5 from './asset5.jpeg'
+import asset10 from './asset10.jpeg'
+
 
 export {
+  Logo,
+  asset22,
+  asset6,
+  asset4,
+  asset5,
+  asset10,
   logo,
+  asset3,
+  asset15,
+  asset16,
+  asset17,
+  asset18,
+  asset20,
+  asset25,
+  asset23,
+  asset13,
+  hero,
   backend,
   creator,
   mobile,
